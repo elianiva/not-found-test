9a7c87e6d5f8aff3c6f79010406e67daf74ff41e
elianiva <git@elianiva.my.id>
2026-05-07 00:33:36 +0700

    fix: type import

--- load-context.ts
+++ load-context.ts
@@ -1,21 +1,7 @@
-import { type PlatformProxy } from "wrangler";
-
-type GetLoadContextArgs = {
-  request: Request;
-  context: {
-    cloudflare: Omit<PlatformProxy<Env>, "dispose" | "caches" | "cf"> & {
-      caches: PlatformProxy<Env>["caches"] | CacheStorage;
-      cf: Request["cf"];
-    };
-  };
-};
-
 declare module "@react-router/dev/vite/cloudflare" {
-  interface AppLoadContext extends ReturnType<typeof getLoadContext> {
-    // This will merge the result of `getLoadContext` into the `AppLoadContext`
-  }
+  interface AppLoadContext extends ReturnType<typeof getLoadContext> {}
 }
 
-export function getLoadContext({ context }: GetLoadContextArgs) {
+export function getLoadContext({ context }: { request: Request; context: any }) {
   return context;
 }
